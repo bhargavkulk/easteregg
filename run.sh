9b4b82c7b112b133285d67f709f18445bd709e0c
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-04-14 11:23:57 -0600

    checking rustup status

--- run.sh
+++ run.sh
@@ -3,11 +3,18 @@ set -e -x
 if [ ! -d skia ]; then
     git clone https://skia.googlesource.com/skia.git
 fi
+if [ ! -d egglog ]; then
+    git clone https://github.com/egraphs-good/egglog/
+fi
+
+
 cd skia
 python3 tools/git-sync-deps
 python3 bin/fetch-ninja
 ./bin/gn gen out/debug
 ninja -C out/debug dm skp_parser
+
+rustup show
 
 # REPORT LOLW
 
