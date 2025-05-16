02e5e5e9becf4dc864a383fe7a64920c593999ab
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-05-15 20:21:10 -0600

    shutting this down for now

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ set -e -x
 # if [ ! -d skia ]; then
 #     git clone https://skia.googlesource.com/skia.git
 # fi
-if [ ! -d egglog ]; then
-    git clone https://github.com/egraphs-good/egglog/
-fi
+#if [ ! -d egglog ]; then
+#    git clone https://github.com/egraphs-good/egglog/
+#fi
 
-rm -rf eegg err opt report
+#rm -rf eegg err opt report
 
 # cd skia
 # python3 tools/git-sync-deps
@@ -16,22 +16,19 @@ rm -rf eegg err opt report
 # ninja -C out/debug dm skp_parser
 # cd ..
 
-cd egglog
-cargo install --locked cargo-nextest --version 0.9.85
-cargo build
-cd ..
+#cd egglog
+#cargo install --locked cargo-nextest --version 0.9.85
+#cargo build
+#cd ..
 
 mkdir -p report
 touch report/index.html
 
-python3 -m venv venv
+#python3 -m venv venv
 
-$(pwd)/venv/bin/python -m pip install pygments sexpdata
-$(pwd)/venv/bin/python -m playwright install
+#$(pwd)/venv/bin/python -m pip install pygments sexpdata
+#$(pwd)/venv/bin/python -m playwright install
 ## SHOULD PROBABLY CACHE THE SKPS TALK TO PAVEL ABOUT THIS
-$(pwd)/venv/bin/python skp2egg.py json eegg err
-$(pwd)/venv/bin/python opt.py egg eegg opt
-$(pwd)/venv/bin/python gen_report.py json eegg err opt report
-
-ls eegg
-ls err
+#$(pwd)/venv/bin/python skp2egg.py json eegg err
+#$(pwd)/venv/bin/python opt.py egg eegg opt
+#$(pwd)/venv/bin/python gen_report.py json eegg err opt report
