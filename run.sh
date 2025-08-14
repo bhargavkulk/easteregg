06408c3a7a0732e927aef785851c011ca95040e1
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-08-14 15:31:00 -0600

    should work

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e -x
-# if [ ! -d skia ]; then
-#     git clone https://skia.googlesource.com/skia.git
-# fi
-# if [ ! -d egglog ]; then
-#     git clone https://github.com/egraphs-good/egglog/
-# fi
+set -o pipefail
+
+export PATH=~/.cargo/bin:$PATH
+
+rustup update
 
 if [ -d egglog ]; then
     rm -rf egglog
@@ -13,12 +12,7 @@ fi
 
 git clone https://github.com/egraphs-good/egglog/
 
-rustup override set 1.87.0
-
-cd egglog/
-cargo build -Znext-lockfile-bump
-cd ..
-rm -rf report
+cargo build --manifest-path egglog/Cargo.toml
 
 # cd skia
 # python3 tools/git-sync-deps
@@ -27,16 +21,7 @@ rm -rf report
 # ninja -C out/debug dm skp_parser
 # cd ..
 
-#cd egglog
-#cargo install --locked cargo-nextest --version 0.9.85
-#cargo build
-#cd ..
-
 python3 -m venv venv
 $(pwd)/venv/bin/python -m pip install uv
 $(pwd)/venv/bin/python -m uv sync
 $(pwd)/venv/bin/python -m uv run make_report.py new-bench/json rsrc report
-
-#$(pwd)/venv/bin/python -m pip install playwright sexpdata yattag skia_python-138.0rc1-cp312-cp312-manylinux_2_28_x86_64.whl
-#$(pwd)/venv/bin/python -m playwright install
-#$(pwd)/venv/bin/python make_report.py new-bench/json rsrc report
