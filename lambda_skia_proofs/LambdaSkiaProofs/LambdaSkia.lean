3d7e219d747eaaf8dfe3b0e06970e5cd0d2afea0
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-09-10 10:46:09 -0600

    new rule??? PogU

--- lambda_skia_proofs/LambdaSkiaProofs/LambdaSkia.lean
+++ lambda_skia_proofs/LambdaSkiaProofs/LambdaSkia.lean
@@ -14,17 +14,23 @@ def Transform: Type := Point -> Point
 -- Color := (alpha, red, green, blue)
 def Color : Type := (Float × Float × Float × Float)
 
+axiom isOpaque (c : Color) : Prop
+
 def Transparent : Color := (0.0, 0.0, 0.0, 0.0)
 
 -- Geometry is a collection of points
 abbrev Geometry := Point -> Bool
 
+def intersect (g1 g2 : Geometry) : Geometry :=
+  fun pt => g1 pt && g2 pt
+
 -- Style changes the geometry
 abbrev Style := Geometry -> Geometry
+def Fill : Style := fun g => g
 
 -- Color Filters
 abbrev ColorFilter := Color -> Color
-
+def idColorFilter : ColorFilter := fun c => c
 -- Paintdraw defines how a geometry is painted
 abbrev PaintDraw := Style × (Point -> Color) × ColorFilter
 
@@ -47,6 +53,17 @@ axiom SrcOver_right_transparent :
 @[grind]
 axiom SrcOver_associative :
   forall c₁ c₂ c₃ : Color, SrcOver (SrcOver c₁ c₂) c₃ = SrcOver c₁ (SrcOver c₂ c₃)
+
+axiom DstIn: BlendMode
+@[grind]
+axiom DstIn_left_transparent:
+  forall c : Color, DstIn Transparent c = Transparent
+@[grind]
+axiom DstIn_right_transparent:
+  forall c : Color, DstIn c Transparent = Transparent
+@[grind]
+axiom DstIn_right_opaque:
+  forall c1 c2 : Color, isOpaque c2 -> DstIn c1 c2 = c1
 
 -- blend mode src:
 axiom Src : BlendMode
@@ -73,17 +90,17 @@ axiom applyAlpha_on_transparent :
   forall a : Float, applyAlpha a Transparent = Transparent
 
 @[grind]
-noncomputable def blend  (l₁ l₂ : Layer) (pb: PaintBlend) (clip : Geometry) : Layer :=
+noncomputable def blend  (l₁ l₂ : Layer) (pb: PaintBlend) : Layer :=
   let (α, bm) := pb
-  fun pt => if (clip pt) then bm (l₁ pt) (applyAlpha α (l₂ pt)) else (l₁ pt)
+  fun pt => bm (l₁ pt) (applyAlpha α (l₂ pt))
 
   -- rasterizes a geometry into a layer
 @[grind]
-noncomputable def raster (shape: Geometry) (paint: PaintDraw) (t: Transform) : Layer :=
+noncomputable def raster (shape: Geometry) (paint: PaintDraw) (t: Transform) (clip: Geometry): Layer :=
   let (style, color, color_filter) := paint
   fun pt =>
   let pt := (t pt)
-  if (style shape) pt then color_filter (color pt) else Transparent
+  if (style shape) pt && (clip pt) then color_filter (color pt) else Transparent
 
 -- Now we define layers
 -- Empty()
@@ -95,7 +112,7 @@ def EmptyLayer : Layer := (fun _ => Transparent)
 -- l₂ top layer
 @[grind]
 noncomputable def SaveLayer (l₁ l₂ : Layer) (pb : PaintBlend) : Layer :=
-  blend l₁ l₂ pb (fun _ => true)
+  blend l₁ l₂ pb
 
 theorem empty_SrcOver_SaveLayer_is_Empty l:
   SaveLayer l EmptyLayer (1.0, SrcOver) = l := by grind
@@ -103,7 +120,7 @@ theorem empty_SrcOver_SaveLayer_is_Empty l:
 -- now we define draw
 @[grind]
 noncomputable def Draw (l : Layer) (g : Geometry) (pd : PaintDraw) (pb : PaintBlend) (t: Transform)(clip : Geometry): Layer :=
-  blend l (raster g pd t) pb clip
+  blend l (raster g pd t clip) pb
 
 theorem lone_draw_inside_opaque_srcover_savelayer
   (bottom : Layer) (g : Geometry) (pd : PaintDraw) (α: Float) (c : Geometry) (t: Transform):
@@ -130,3 +147,26 @@ theorem last_draw_inside_opaque_srcover_savelayer
   (l₁ l₂ : Layer) (g c : Geometry) (pd : PaintDraw) (α : Float) (t : Transform):
   SaveLayer l₁ (Draw l₂ g pd (α, SrcOver) t c) (1.0, SrcOver) = Draw (SaveLayer l₁ l₂ (1.0, SrcOver)) g pd (α, SrcOver) t c := by
   grind
+
+theorem something g1 pd1 a1 c1 t g2 c2 c (H: isOpaque c):
+  SaveLayer (Draw EmptyLayer g1 pd1 (a1, SrcOver) t c1)
+  (Draw EmptyLayer g2 (Fill, fun _ => c, idColorFilter) (1.0, SrcOver) t c2) (1.0, DstIn)
+  =
+  Draw EmptyLayer g1 pd1 (a1, SrcOver) t (intersect c1 (intersect g2 c2)) := by
+  simp [SaveLayer, Draw, blend]
+  ext pt
+  simp [raster, EmptyLayer, intersect]
+
+  cases (c1 (t pt))
+  · grind
+  · simp [applyAlpha_opaque]
+    cases (c2 (t pt))
+    · grind
+    · simp [SrcOver_right_transparent]
+      simp [Fill, idColorFilter]
+      cases (pd1.fst g1 (t pt))
+      · simp [applyAlpha_on_transparent]
+        simp [DstIn_left_transparent]
+      · cases (g2 (t pt))
+        · grind
+        · grind
