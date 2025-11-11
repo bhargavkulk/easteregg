2b6e7c8a5fe84c6d727b40e72323f37875bff0fe
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-11-10 17:01:50 -0700

    idiot

--- src/skp_compiler.py
+++ src/skp_compiler.py
@@ -240,9 +240,8 @@ def compile_skp_to_lskia(commands: list[dict[str, Any]]) -> Layer:
                 op: ClipOp = command_data['op']
                 push_clip(RRect(*([i / 1.0 for i in coords + ltrb_radii])), op)
             case 'ClipPath':
-                raise NotImplementedError('Pausing this for now')
-                # op: ClipOp = command_data['op']
-                # push_clip(Path(i), op)
+                op: ClipOp = command_data['op']
+                push_clip(Path(i), op)
             case 'Concat44':
                 matrix: list[float] = [i for s in command_data['matrix'] for i in s]
                 push_transform(matrix)
