6e4414e2163bbea8acfd127caad09c9bd13526c8
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2026-01-09 12:00:43 -0700

    Removing dstin check

--- src/verify.py
+++ src/verify.py
@@ -30,7 +30,7 @@ def verify_color_filter(colorfilter: dict):
             assert '01_uint' in colorfilter['values']  # blend mode
 
         case 'SkRuntimeColorFilter':
-            assert False, "Searching for luma filter"
+            assert False, 'Searching for luma filter'
             # 00_int -> stable key (figure this out)
             # 01_string -> the runtime function (exists only if 00_int is 0)
             # 02_bytearray -> funiforms
@@ -216,7 +216,7 @@ def verify_paint(paint: dict):
             case 'blendMode':
                 assert isinstance(value, str)
                 verify_blend_mode(value)
-                assert value != 'DstIn', 'DstIn blend mode encountered'
+                # assert value != 'DstIn', 'DstIn blend mode encountered'
             case 'blur':
                 # use this to make shadows, do a difference clip, to just draw the borders
 
